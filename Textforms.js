19091b1f2673078852cd514bb9ef62ac4bb3e4ef
krishna-khatke04 <krishnakhatke575@gmail.com>
2025-07-07 11:02:37 +0530

    Update Textforms.js

--- Textforms.js
+++ Textforms.js
@@ -20,9 +20,7 @@ export default function Textforms(props) {
         props.showAlert("Cleared Text","Success")
     }
     const handleCopy=()=>{
-      var text=document.getElementById("myBox");
-      text.select();
-      navigator.clipboard.writeText(text.value);
+      navigator.clipboard.writeText(text);
       props.showAlert("Text copied","Success");
     }
      const handleExtraSpaces = () => {
